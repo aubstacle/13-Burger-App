4df6162a5854a34206e2f31efe3184dfe39f4648
Aubrey Polk <aubstacle3@gmail.com>
2020-10-10 00:18:01 -0400

    making heroku work

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Freeman5",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Freeman5",
+//   database: "burgers_db"
+// });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// // Export connection for our ORM to use.
+// module.exports = connection;
