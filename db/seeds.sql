1bfb1052bd5faf46353c09edb5c6a8a135d6c2aa
Aubrey Polk <aubstacle3@gmail.com>
2020-10-21 09:29:37 -0400

    saved schema and seeds from workbench

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cowboy Style Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', FALSE);
